6b858b0e385ba7e2795ebd64de02ebf576a327f5
harumhoirunisa <0110124058@student.nurulfikri.ac.id>
2025-03-13 11:41:46 +0700

    upload file 3

---
+++ pertemuan3/task/page/projects.php
@@ -0,0 +1,46 @@
+        <!-- Projects Section-->
+            <section class="py-5">
+                <div class="container px-5 mb-5">
+                    <div class="text-center mb-5">
+                        <h1 class="display-5 fw-bolder mb-0"><span class="text-gradient d-inline">Projects</span></h1>
+                    </div>
+                    <div class="row gx-5 justify-content-center">
+                        <div class="col-lg-11 col-xl-9 col-xxl-8">
+                            <!-- Project Card 1-->
+                            <div class="card overflow-hidden shadow rounded-4 border-0 mb-5">
+                                <div class="card-body p-0">
+                                    <div class="d-flex align-items-center">
+                                        <div class="p-5">
+                                            <h2 class="fw-bolder">Project Kalkulator Bangun Ruang</h2>
+                                            <p>Kalkulator ini dibuat untuk memenuhi tugas akhir dari mata kuliah Dasar-Dasar Pemrograman, projek ini dibuat dengan beberapa teman saya untuk memudahkan dalam menghitung bangun ruang.</p>
+                                        </div>
+                                        <img class="img-fluid" src="https://dummyimage.com/300x400/343a40/6c757d" alt="..." />
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Project Card 2-->
+                            <div class="card overflow-hidden shadow rounded-4 border-0">
+                                <div class="card-body p-0">
+                                    <div class="d-flex align-items-center">
+                                        <div class="p-5">
+                                            <h2 class="fw-bolder">Project Web Sekolah</h2>
+                                            <p>Projek ini dibuat untuk memenuhi tugas akhir mata kuliah Pemrograman Web. Tugas ini dikerjakan bersama teman sekelompok saya. pada web ini ada beberapa fitur untuk melihat keseluruhan tentang sekolah.</p>
+                                        </div>
+                                        <img class="img-fluid" src="https://dummyimage.com/300x400/343a40/6c757d" alt="..." />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- Call to action section-->
+            <section class="py-5 bg-gradient-primary-to-secondary text-white">
+                <div class="container px-5 my-5">
+                    <div class="text-center">
+                        <h2 class="display-4 fw-bolder mb-4">Mari Belajar Bersama!</h2>
+                        <a class="btn btn-outline-light btn-lg px-5 py-3 fs-6 fw-bolder" href="contact.html">Hubungi Saya</a>
+                    </div>
+                </div>
+            </section>
+        
---
+++ pertemuan3/task/page/resume.php
@@ -0,0 +1,138 @@
+        <!-- Page Content-->
+            <div class="container px-5 my-5">
+                <div class="text-center mb-5">
+                    <h1 class="display-5 fw-bolder mb-0"><span class="text-gradient d-inline">Resume</span></h1>
+                </div>
+                <div class="row gx-5 justify-content-center">
+                    <div class="col-lg-11 col-xl-9 col-xxl-8">
+                        <!-- Experience Section-->
+                        <section>
+                            <div class="d-flex align-items-center justify-content-between mb-4">
+                                <h2 class="text-primary fw-bolder mb-0">Experience</h2>
+                                <!-- Download resume button-->
+                                <!-- Note: Set the link href target to a PDF file within your project-->
+                                <a class="btn btn-primary px-4 py-3" href="#!">
+                                    <div class="d-inline-block bi bi-download me-2"></div>
+                                    Download Resume
+                                </a>
+                            </div>
+                            <!-- Experience Card 1-->
+                            <div class="card shadow border-0 rounded-4 mb-5">
+                                <div class="card-body p-5">
+                                    <div class="row align-items-center gx-5">
+                                        <div class="col text-center text-lg-start mb-4 mb-lg-0">
+                                            <div class="bg-light p-4 rounded-4">
+                                                <div class="text-primary fw-bolder mb-2">2023 - 2024</div>
+                                                <div class="small fw-bolder">Pemandu Outbond</div>
+                                                <div class="small text-muted">Pelita Desa</div>
+                                                <div class="small text-muted">Ketapang, Ciseeng</div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-8"><div>Memandu anak TK/SD untuk belajar membuat batik, seperti menjelaskan alat-alat, bahan, dan cara menggunakan alatnya dengan benar.</div></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Experience Card 2-->
+                            <div class="card shadow border-0 rounded-4 mb-5">
+                                <div class="card-body p-5">
+                                    <div class="row align-items-center gx-5">
+                                        <div class="col text-center text-lg-start mb-4 mb-lg-0">
+                                            <div class="bg-light p-4 rounded-4">
+                                                <div class="text-primary fw-bolder mb-2">2023 - 2024</div>
+                                                <div class="small fw-bolder">Pelayan rumah Makan</div>
+                                                <div class="small text-muted">Saung Sultan, Tansu Platation</div>
+                                                <div class="small text-muted">Ciseeng, parung</div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-8"><div>Membantu melayani customer, mengantarkan makanan, membersihkan meja, dan mengelap peralatan yang sudah dibersihkan.</div></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                        <!-- Education Section-->
+                        <section>
+                            <h2 class="text-secondary fw-bolder mb-4">Pendidikan</h2>
+                            <!-- Education Card 1-->
+                            <div class="card shadow border-0 rounded-4 mb-5">
+                                <div class="card-body p-5">
+                                    <div class="row align-items-center gx-5">
+                                        <div class="col text-center text-lg-start mb-4 mb-lg-0">
+                                            <div class="bg-light p-4 rounded-4">
+                                                <div class="text-secondary fw-bolder mb-2">2022 - 2024</div>
+                                                <div class="mb-2">
+                                                    <div class="small fw-bolder">SMA Pewaris Peradaban</div>
+                                                    <div class="small text-muted">Ciseeng, Bogor</div>
+                                                </div>
+                                                <div class="fst-italic">
+                                                    <div class="small text-muted">Siswa</div>
+                                                    <div class="small text-muted">Jurusan IPS</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-8"><div>Disekolah ini saya diajarkan untuk belajar mandiri dalam segala hal, ada beberapa program khusu yang membantu melatih kemandirian tersebut, salah satunya adalah hiking gunung, dimana disini kita dilatih untuk menjaga diri ketika dialam luar, bertanggung jawab terhadap diri sendiri dan sekitar.</div></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Education Card 2-->
+                            <div class="card shadow border-0 rounded-4 mb-5">
+                                <div class="card-body p-5">
+                                    <div class="row align-items-center gx-5">
+                                        <div class="col text-center text-lg-start mb-4 mb-lg-0">
+                                            <div class="bg-light p-4 rounded-4">
+                                                <div class="text-secondary fw-bolder mb-2">2024 - sekarang</div>
+                                                <div class="mb-2">
+                                                    <div class="small fw-bolder">STT Terpadu Nurul Fikri</div>
+                                                    <div class="small text-muted">Srengseng Sawah</div>
+                                                </div>
+                                                <div class="fst-italic">
+                                                    <div class="small text-muted">Mahasiswa</div>
+                                                    <div class="small text-muted">Sistem Informasi</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-8"><div>selama 2 semester ini saya banyak belajar tentang codingan, desin, kerjasama dengan kelompok, mengatur waktu dengan baik agar tidak bentrok dengan tugas atau kerjaan yang lain.</div></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                        <!-- Divider-->
+                        <div class="pb-5"></div>
+                        <!-- Skills Section-->
+                        <section>
+                            <!-- Skillset Card-->
+                            <div class="card shadow border-0 rounded-4 mb-5">
+                                <div class="card-body p-5">
+                                    <!-- Professional skills list-->
+                                    <div class="mb-5">
+                                        <div class="d-flex align-items-center mb-4">
+                                            <div class="feature bg-primary bg-gradient-primary-to-secondary text-white rounded-3 me-3"><i class="bi bi-tools"></i></div>
+                                            <h3 class="fw-bolder mb-0"><span class="text-gradient d-inline">Professional Skills</span></h3>
+                                        </div>
+                                        <div class="row row-cols-1 row-cols-md-3 mb-4">
+                                            <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">Web development</div></div>
+                                            <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">Komunikasi Efektif</div></div>
+                                            <div class="col"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">User Interface Design</div></div>
+                                        </div>
+                                    <!-- Languages list-->
+                                    <div class="mb-0">
+                                        <div class="d-flex align-items-center mb-4">
+                                            <div class="feature bg-primary bg-gradient-primary-to-secondary text-white rounded-3 me-3"><i class="bi bi-code-slash"></i></div>
+                                            <h3 class="fw-bolder mb-0"><span class="text-gradient d-inline">Languages</span></h3>
+                                        </div>
+                                        <div class="row row-cols-1 row-cols-md-3 mb-4">
+                                            <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">HTML</div></div>
+                                            <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">CSS</div></div>
+                                            <div class="col"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">JavaScript</div></div>
+                                        </div>
+                                        <div class="row row-cols-1 row-cols-md-3">
+                                            <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">Python</div></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </main>
+        
